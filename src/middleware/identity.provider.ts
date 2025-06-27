857277710b9d2e5c5b5a6b0fb78a5bee5fc035df
pitmov <morph@8b2efe3b4820>
2025-06-27 13:16:18 +0000

    Morph: Remove console logs 2

--- src/middleware/identity.provider.ts
+++ src/middleware/identity.provider.ts
@@ -3,7 +3,6 @@ import { Request } from 'express';
 export class IdentityProvider {
   getUserId(req: Request): string | null {
     const userId = req.header('identity-user-id');
-    console.log(`[IdentityProvider] Extracted user ID from headers: ${userId}`);
     return userId || null;
   }
 }
